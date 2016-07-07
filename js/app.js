4013f5d2d34a0243e06b38026c9ef79fe285026d
jeff szajner <jszajner@yahoo.com>
2016-07-06 21:15:26 -0600

    Completing the JS structure but not the functions

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*******************************************
+STEP 1
+before document ready we are defining all the functions and we explain what they should be doing when used
+********************************************/
+
+/*function to add items in the list*/
+function addItems() {}
+
+/*function to check items in the list*/
+function checkItems() {}
+
+/*function to delete one item at a time*/
+function deleteOneItem() {}
+
+/*function for button to clear the list*/
+function clearList() {}
+
+/********************************************
+STEP 2
+Inside document ready we are calling all the functions (we used them) and connect them with the containers in HTML (for example the #add-button from HTML will be connected with the addItem function)
+********************************************/
+
+
+$(document).ready(function () {
+
+    /*on click on the #addItem activate function add item*/
+
+    /*on click on the .checkbox activate function check items*/
+
+    /*on click on the .delete activate function delete one item*/
+
+    /*on click on the #clearAll activate function clear the list*/
+});
